6f7ee39e1aed83446e8cde8fe777ec3772503894
Marco Molari <mmolari91@gmail.com>
2021-12-06 16:36:06 +0100

    feat: moved minipolish installation to conda env

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,9 @@
 # create and activate conda environment
-conda env create -f conda_env.yml
-
-# install minipolish
-pip3 install git+https://github.com/rrwick/Minipolish.git
+# conda env create -f conda_env.yml
 
 # Download miniasm_and_minipolish.sh script
 wget -N -O scripts/miniasm_and_minipolish.sh \
     https://raw.githubusercontent.com/rrwick/Minipolish/main/miniasm_and_minipolish.sh
 
-# TODO: activate the environment
-# TODO: update nextflow
+# TODO: nextflow self-update
 # TODO: set the location of the environment in the nextflow.config file
