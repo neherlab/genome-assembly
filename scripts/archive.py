1c3bf2f17bb59957a804aac1d5e2856839adfea5
Marco Molari <mmolari91@gmail.com>
2022-03-01 12:45:17 +0100

    feat: completed main part of archiviation script

--- scripts/archive.py
+++ scripts/archive.py
@@ -14,12 +14,13 @@ from shutil import ExecError
 import subprocess
 import numpy as np
 import pandas as pd
+import time
 
 # dest = pathlib.Path("~/GROUP/data/2022_nanopore_sequencing/")
 dest = pathlib.Path("archive")
-raw_dir = dest / "raw"
-bc_dir = dest / "basecalled"
-exp_dir = dest / "experiments"
+raw_main_dir = dest / "raw"
+bc_main_dir = dest / "basecalled"
+exp_main_dir = dest / "experiments"
 
 
 def extract_flowcell_run_id(data_fld):
@@ -115,7 +116,9 @@ def run_command(command):
         print(subp.stderr.decode())
         raise ExecError(f"command {' '.join(command)} failed")
     else:
-        print(subp.stdout.decode())
+        stdout = subp.stdout.decode()
+        if len(stdout) > 0:
+            print(stout)
 
 
 def create_fast5_archive(archive_fld, fast5_fld):
@@ -132,6 +135,62 @@ def create_fast5_archive(archive_fld, fast5_fld):
     ] + fast5_files
     run_command(command)
     return archive_file
+
+
+def make_read_only(files):
+    """given a list of files, runs chmod 444 to make them read-only."""
+    file_names = [str(f) for f in files]
+    run_command(["chmod", "444"] + file_names)
+
+
+def mkdir_check_nonexistent(dir_path):
+    """Check that a directory does not exist, and if so then creates it."""
+    if dir_path.is_dir():
+        raise FileExistsError(
+            f"""The folder {dir_path} already exists.
+        Remove it if you want to overwrite it."""
+        )
+    dir_path.mkdir()
+
+
+def single_experiment_readme(subdir, df_row):
+    """creates a README.txt file for the single experiment"""
+    message = [
+        "The reads for this condition:",
+        f"\texperiment tag: {df_row.experiment_id}",
+        f"\tdate: {df_row.date}",
+        f"\tvial: {df_row.vial}",
+        f"\ttimepoint: {df_row.timepoint}",
+        "were produced in the sequencing run:",
+        f"\tflowcell run: {df_row.flowcell_run_id}",
+        f"\tbarcode: {df_row.barcode}",
+    ]
+    rdm_file = subdir / "README.txt"
+    with open(rdm_file, "w") as f:
+        f.write("\n".join(message))
+
+
+def lock_file(f):
+    """creates a file.lock to signal the lock of the file"""
+    lock_f = pathlib.Path(str(f) + ".lock")
+    is_locked = False
+    t = 0
+    while t < 50:
+        if lock_f.is_file():
+            t += 1
+            time.sleep(5)
+        else:
+            with open(lock_f, "w") as lf:
+                lf.write(f"lock file created on {datetime.date.today().isoformat()}")
+            return True
+    raise ExecError(f"maximum waiting time exceeded when trying to lock {f}")
+
+
+def unlock_file(f):
+    """Unlocks a file by removing the corresponding lock file"""
+    lock_f = pathlib.Path(str(f) + ".lock")
+    assert lock_f.is_file(), "Lock file missing"
+    os.remove(lock_f)
 
 
 if __name__ == "__main__":
@@ -186,14 +245,19 @@ if __name__ == "__main__":
         exit(0)
 
     # if the csv file exists then load it and check if it is valid
-    df = pd.read_csv(sample_info_file)
+    read_sample_info_kwargs = {"dtype": {"barcode": int, "vial": str, "timepoint": str}}
+    df = pd.read_csv(sample_info_file, **read_sample_info_kwargs)
     print(df)
     check_valid(df)
 
     # ask for confirmation before data archiviation
     answer = input(
-        """Confirm? [y/yes to accept]
-    Nb: only barcodes where valid==True will be transferred.\n"""
+        "\n".join(
+            [
+                "Confirm? [y/yes to accept]",
+                "Nb: only barcodes where valid==True will be transferred.\n",
+            ]
+        )
     )
     if not (answer.lower() in ["y", "yes"]):
         print("Aborting data archiviation.")
@@ -207,16 +271,13 @@ if __name__ == "__main__":
     # name for fast5 files storage folder
     flow_id = df["flowcell_run_id"][0]
     today = datetime.date.today().isoformat()
-    archive_fld = raw_dir / f"{today}_{flow_id}"
+    flow_run_tag = f"{today}_{flow_id}"
+    archive_fld = raw_main_dir / flow_run_tag
 
     # check that folder does not already exist and create it
-    if archive_fld.is_dir():
-        raise FileExistsError(
-            f"""The folder {archive_fld} already exists.
-        Remove it if you want to overwrite it."""
-        )
-    archive_fld.mkdir()
+    mkdir_check_nonexistent(archive_fld)
 
+    # list entries to archive
     print("archiving the following entries:")
     print(df)
 
@@ -233,18 +294,38 @@ if __name__ == "__main__":
     # TODO: add README
 
     # changing permissions
-    print(
-        f"changing file permissions to read-only to {fast5_archive} and {stats_file}..."
-    )
-    run_command(["chmod", "444", str(fast5_archive), str(stats_file)])
+    change_permissions_to = [fast5_archive, stats_file]
+    print("Changing permissions to read-only (chmod 444) for:")
+    print(*change_permissions_to)
+    make_read_only(change_permissions_to)
 
     # TODO: add README/section in group wiki
 
     # -------------- archive fastq reads ---------------------
 
-    # TODO: next: create a folder for the sequencing run with the same name, and
-    # move reads with barcode names (fastq.gz files) and save stats file.
-    # and change permissions to read-only
+    # create folder to store reads (check that it does not exist already)
+    fastq_to_fld = bc_main_dir / flow_run_tag
+    mkdir_check_nonexistent(fastq_to_fld)
+
+    # transfer fastq reads to basecalled folder
+    fastq_from_fld = data_fld / "basecalled"
+    print(f"copy selected barcodes from {fastq_from_fld} to {fastq_to_fld}")
+    fastq_to_files = {}  # dictionary with files destinations
+    for bc in df.barcode.values:
+        print(f"processing barcode {bc}")
+        fastq_from_file = fastq_from_fld / f"barcode{int(bc):02d}.fastq.gz"
+        assert fastq_from_file.is_file(), f"file {fastq_from[bc]} does not exist."
+        fastq_to_files[bc] = fastq_to_fld / f"barcode{int(bc):02d}.fastq.gz"
+        # copy fastq files
+        run_command(["cp", str(fastq_from_file), str(fastq_to_files[bc])])
+
+    print("adding sample.csv")
+    sample_info_file = fastq_to_fld / "sample.csv"
+    df.to_csv(sample_info_file)
+
+    print("changing file permissions")
+    readonly_files = [str(f) for f in fastq_to_files.values()] + [str(sample_info_file)]
+    make_read_only(readonly_files)
 
     # -------------- create experiment database with links ---------------------
 
@@ -252,17 +333,50 @@ if __name__ == "__main__":
     pairs = df[["experiment_id", "date"]].value_counts().index.to_list()
     for exp_id, date in pairs:
 
+        print(f"processing experiment {exp_id} date {date}")
+
         # select only data
         mask = (df.experiment_id == exp_id) & (df.date == date)
-        sdf = df[mask]
+        sdf = df[mask].copy()
 
-    # TODO: check that file does not already exist
-    # exp_subdir = exp_dir /
-    # print(f"creating folder")
+        # define experiment folder and create it if it does not exist
+        exp_tag = f"{date}_{exp_id}"
+        exp_dir = exp_main_dir / exp_tag
+        exp_dir.mkdir(exist_ok=True)
 
-    # TODO: create subfolder for basecalled reads, name = basecalling run id
-    # TODO: move fastq file with all basecalled read. Rename according to
+        # for every barcode
+        for idx, row in sdf.iterrows():
+            bc, vial, tp = row.barcode, row.vial, row.timepoint
+            print(f"processing barcode {bc}, vial {vial}, timepoint {tp}")
 
-    # TODO: create experiment subfolder
-    # TODO: link files
+            # create experiment vial/timepoint subdirectory
+            exp_subdir = exp_dir / f"vial_{vial}" / f"time_{tp}"
+            assert not exp_subdir.is_dir(), f"the directory {exp_subdir} already exists"
+            exp_subdir.mkdir(parents=True)
+
+            # create symbolic link to reads, and make it read-only
+            link_file = exp_subdir / "reads.fastq.gz"
+            command = ["ln", "-s", fastq_to_files[bc].resolve(), str(link_file)]
+            run_command(command)
+            make_read_only([str(link_file)])
+
+            single_experiment_readme(exp_subdir, row)
+
+        # check if sample.csv already exists, if so merge and overwrite
+        sample_info = exp_dir / "sample.csv"
+        if sample_info.is_file():
+            print(f"appending to {sample_info}")
+            lock_file(sample_info)
+            old_df = pd.read_csv(sample_info, **read_sample_info_kwargs)
+            new_df = pd.concat([sdf, old_df], ignore_index=True)
+            run_command(["chmod", "666", str(sample_info)])
+            new_df.to_csv(sample_info, index=False)
+            unlock_file(sample_info)
+        else:
+            print(f"creating {sample_info}")
+            sdf.to_csv(sample_info, index=False)
+
+        # making the file read-only
+        run_command(["chmod", "444", str(sample_info)])
+
     # TODO: transfer prokka folder (which files?) if existing
